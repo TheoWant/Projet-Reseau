55ce1beb07b214ee3bae27d5067b814c6f040389
WoojysAkak <81567975+WoojysAkak@users.noreply.github.com>
2023-04-27 21:28:30 +0200

    Update main.cpp

--- Projet Reseau/main.cpp
+++ Projet Reseau/main.cpp
@@ -91,3 +91,107 @@ int main()
 
 	return 0;
 }
+
+DWORD WINAPI ClientToServerThread(LPVOID lpParam) {
+	WSADATA WSAData;
+	SOCKET sock;
+	SOCKADDR_IN socketInfo;
+	std::string s;
+	char input[1024 + 1];
+
+	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+	WNDCLASS wc = { };
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = NULL;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	// Create the window.
+
+	HWND hWnd = CreateWindowEx(
+		0,                              // Optional window styles.
+		CLASS_NAME,                     // Window class
+		L"Learn to Program Windows",    // Window text
+		WS_OVERLAPPEDWINDOW,            // Window style
+
+		// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+		NULL,       // Parent window    
+		NULL,       // Menu
+		NULL,       // Instance handle
+		NULL        // Additional application data
+	);
+
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) == 0) {}
+
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	InetPton(AF_INET, L"127.0.0.1", &socketInfo.sin_addr.s_addr);
+	socketInfo.sin_family = AF_INET;
+	socketInfo.sin_port = htons(25565);
+
+	Sleep(1000);
+	int connection = connect(sock, (SOCKADDR*)&socketInfo, sizeof(socketInfo)); // connect to server
+
+	MSG msg;
+
+	while (GetMessage(&msg, hWnd, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return 0;
+}
+
+LRESULT WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	SOCKET Accept;
+
+	switch (uMsg)
+	{
+
+	case WM_SOCKET:
+		// Determine whether an error occurred on the
+		// socket by using the WSAGETSELECTERROR() macro
+		if (WSAGETSELECTERROR(lParam))
+		{
+			// Display the error and close the socket
+			closesocket((SOCKET)wParam);
+			break;
+		}
+		// Determine what event occurred on the socket
+		switch (WSAGETSELECTEVENT(lParam))
+		{
+		case FD_READ:
+		{
+			// Receive data from the socket in wParam
+			char buffer[1024];
+			int message = recv((SOCKET)wParam, buffer, sizeof(buffer), 0);
+			if (message == 1)
+			{
+				std::cout << "Update la string puis la send";
+				//send((SOCKET)wParam, ClientSide::XYDown.c_str(), ClientSide::XYDown.size(), 0);
+			}
+			break;
+		}
+
+		case FD_CLOSE:
+			// The connection is now closed
+			closesocket((SOCKET)wParam);
+			break;
+		}
+		break;
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+void LaunchThreads()
+{
+	MainThread();
+	HANDLE hThread;
+	DWORD dwThreadId;
+	hThread = CreateThread(NULL, 0, ClientToServerThread, NULL, 0, &dwThreadId);
+}
